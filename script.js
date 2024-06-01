f71dc20ce74686192b1a16d7845b0e85cf7f8530
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-06-01 20:51:45 -0300

    funções e botões

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-
 function load(){document.getElementById('btn').addEventListener('click', function() {
     window.location.href = 'https://www.youtube.com';
 })}
+
+function paginaHome(){document.getElementById('btnLogin').addEventListener('click', function() {
+    window.location.href = "Index.html";
+})}
+
+
+function permitirLogin(nome, senha) {
+    if (nome == "Gustavo" & senha == "1234") {
+        paginaHome();
+    }
+    
+}
